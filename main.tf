d6ff8bec6e8347e1fd1d9cb1ff383135a07b4ac9
Thomas Skill <tskill@us.ibm.com>
2021-12-17 12:50:53 -0600

    rmsens wrapper

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource null_resource setup_gitops {
   depends_on = [null_resource.deployOperator]
 
   provisioner "local-exec" {
-    command = "${local.bin_dir}/igc gitops-module '${local.name}' -n '${nonsensitive(var.namespace)}' --contentDir '${local.yaml_dir}' --serverName '${nonsensitive(var.server_name)}' -l '${local.layer}' --debug"
+    command = "${local.bin_dir}/igc gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}' --debug"
 
     environment = {
       GIT_CREDENTIALS = yamlencode(var.git_credentials)
