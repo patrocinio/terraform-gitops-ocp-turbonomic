2789fb24df439cf562524b51e7fb3f8c5c874e0e
Tom Skill <tskill@us.ibm.com>
2022-02-13 16:01:49 -0600

    workflow and test stage update (#13)

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -1,2 +1,17 @@
 terraform {
+  required_providers {
+    gitops = {
+      source = "cloud-native-toolkit/gitops"
+    }
+  }
+}
+
+provider "gitops" {
+  username = var.git_username
+  token = var.git_token
+  bin_dir  = module.setup_clis.bin_dir
+}
+
+module setup_clis {
+  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
